c16ba5e0ce1d9e5ca950a34d30ba7f9db28ac5a9
Peter Szabo6 <peter_szabo6@epam.com>
2022-07-20 10:58:28 +0200

    Java 9 katas

---
+++ src/main/java/kata/java9/optionalstream/OptionalStream.java
@@ -0,0 +1,21 @@
+package optionalstream;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalStream {
+
+    /**
+     * Implement the getStringList() method using optional.stream() method.
+     */
+
+    public static List<String> getStringList(List<Optional<String>> optionalStringList) {
+        /**
+         Implement your code here.
+         */
+        return null;
+    }
+
+}
+
+
---
+++ src/main/java/kata/java9/setof/SetOf.java
@@ -0,0 +1,27 @@
+package setof;
+
+import java.util.Set;
+
+public class SetOf {
+    /**
+     * Implement getSetOfString() method using Set.of() method.
+     * Implement addAnElementToAStringSet() method, and create a try-catch block to catch
+     * the UnSupportedOperationElement Exception.
+     */
+
+    public static Set<String> getSetOfStrings(String s1, String s2, String s3) {
+        /**
+         * Implement your code here.
+         */
+        return null;
+    }
+
+    public static Set<String> addAnElementToAStringSet(String s1, Set<String> stringSet) {
+        /**
+         * Implement your code here.
+         */
+        return null;
+    }
+
+
+}
