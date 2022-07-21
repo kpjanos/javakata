3af46911091ff13b676c9f841d2d46645f0a4281
Janos Suli <janos_suli@epam.com>
2022-07-21 09:53:34 +0200

    Add Java17 exercises

---
+++ src/main/java/kata/java17/random_number/RandomNumber.java
@@ -0,0 +1,21 @@
+package kata.java17.random_number;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.random.RandomGeneratorFactory;
+import java.util.stream.Collectors;
+
+public class RandomNumber {
+
+    /**
+     * Generate random numbers with the RandomGeneratorFactory class
+     * @param algorithm name of random number generator algorithm
+     * @param streamSize the number of values to generate
+     * @param originNumber the least value that can be produced
+     * @param boundNumber the upper bound (exclusive) for each value produced
+     * @return list of random Integers
+     */
+    public List<Integer> getRandomIntNumbers(String algorithm, int streamSize, int originNumber, int boundNumber) {
+        return new ArrayList<>();
+    }
+}
---
+++ src/main/java/kata/java17/sealed_class/Role.java
@@ -0,0 +1,6 @@
+package kata.java17.sealed_class;
+
+public enum Role {
+    FACTORY_WORKER,
+    SECURITY_GUARD
+}
